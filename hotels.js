babbbca349adf8f02815d8b42eeb7dca457e3db1
Thiago Moraes <thiagoluismoraes@gmail.com>
2020-12-28 03:12:40 -0300

    average

--- hotels.js
+++ hotels.js
@@ -1,27 +1,44 @@
-module.exports.getHotels = () => {
-  return db.query();
-  // return [
-  //   { id: 1, name: "Thiago Hotel" },
-  //   { id: 2, name: "Brucelherme Hotel" },
-  //   { id: 3, name: "Cachorros Hotel" },
-  // ];
+const getHotels = () => {
+    return [
+        { id: 1, name: "Thiago Hotel" },
+        { id: 2, name: "Brucelherme Hotel" },
+        { id: 3, name: "Cachorros Hotel" },
+    ];
 };
 
 const getRatings = () => {
-  return [
-    { hotel_id: 1, client_id: 1, rating: 5 },
-    { hotel_id: 1, client_id: 2, rating: 4 },
-    { hotel_id: 1, client_id: 3, rating: 5 },
-    { hotel_id: 2, client_id: 4, rating: 2 },
-    { hotel_id: 2, client_id: 5, rating: 3 },
-    { hotel_id: 2, client_id: 6, rating: 4 },
-    { hotel_id: 3, client_id: 7, rating: 3 },
-  ];
+    return [
+        { hotel_id: 1, client_id: 1, rating: 5 },
+        { hotel_id: 1, client_id: 2, rating: 4 },
+        { hotel_id: 1, client_id: 3, rating: 5 },
+
+        { hotel_id: 2, client_id: 1, rating: 2 },
+        { hotel_id: 2, client_id: 2, rating: 4 },
+        { hotel_id: 2, client_id: 3, rating: 3 },
+
+        { hotel_id: 3, client_id: 1, rating: 3 },
+        { hotel_id: 3, client_id: 2, rating: 5 },
+        { hotel_id: 3, client_id: 3, rating: 4 },
+    ];
 };
 
-module.exports.getHotelsWithRatings = () => {
-  return module.exports.getHotels().map((hotel) => {
-    hotel.ratings = getRatings().filter((r) => r.hotel_id === hotel.id);
-    return hotel;
-  });
+const getHotelsWithRatings = () => {
+    return getHotels().map((hotel) => {
+        hotel.ratings = getRatings().filter((r) => r.hotel_id === hotel.id);
+        return hotel;
+    });
+
 };
+
+const getHotelsAverageRatings = () => {
+    return getHotels().map(hotel => {
+        const ratings = getRatings().filter((r) => r.hotel_id === hotel.id);
+        hotel.average = ratings.reduce((acc, el) => acc + el.rating, 0 / ratings.length)
+        return hotel
+
+    })
+}
+
+console.log(getHotelsAverageRatings());
+
+module.exports = { getHotels, getRatings, getHotelsWithRatings, getHotelsAverageRatings }
--- hotels.test.js
+++ hotels.test.js
@@ -1,49 +1,61 @@
 const hotels = require("./hotels");
 
 describe("hotels", () => {
-  it("works", () => {
-    expect(1 + 1).toBe(2);
-  });
+    //First Test
+    it("works", () => {
+        expect(1 + 1).toBe(2);
+    });
 
-  it("get hotels with ratings", () => {
-    hotels.getHotels = jest.fn();
-    hotels.getHotels.mockReturnValue([
-      { id: 1, name: "Thiago Hotel" },
-      { id: 2, name: "Brucelherme Hotel" },
-      { id: 3, name: "Cachorros Hotel" },
-    ]);
+    //Second Test
+    it("get hotels with ratings", () => {
+        hotels.getHotels = jest.fn();
+        hotels.getHotels.mockReturnValue([
+            { id: 1, name: "Thiago Hotel" },
+            { id: 2, name: "Brucelherme Hotel" },
+            { id: 3, name: "Cachorros Hotel" },
+        ]);
 
-    // jest.mock(hotels, 'getHotel').toReturn([])
-    const expected = [
-      {
-        id: 1,
-        name: "Thiago Hotel",
-        ratings: [
-          { hotel_id: 1, client_id: 1, rating: 5 },
-          { hotel_id: 1, client_id: 2, rating: 4 },
-          { hotel_id: 1, client_id: 3, rating: 5 },
-        ],
-      },
-      {
-        id: 2,
-        name: "Brucelherme Hotel",
-        ratings: [
-          { hotel_id: 2, client_id: 4, rating: 2 },
-          { hotel_id: 2, client_id: 5, rating: 3 },
-          { hotel_id: 2, client_id: 6, rating: 4 },
-        ],
-      },
-      {
-        id: 3,
-        name: "Cachorros Hotel",
-        ratings: [{ hotel_id: 3, client_id: 7, rating: 3 }],
-      },
-    ];
+        //what you expect
+        const expected = [{
+                id: 1,
+                name: "Thiago Hotel",
+                ratings: [
+                    { hotel_id: 1, client_id: 1, rating: 5 },
+                    { hotel_id: 1, client_id: 2, rating: 4 },
+                    { hotel_id: 1, client_id: 3, rating: 5 },
+                ],
+            },
+            {
+                id: 2,
+                name: "Brucelherme Hotel",
+                ratings: [
+                    { hotel_id: 2, client_id: 1, rating: 2 },
+                    { hotel_id: 2, client_id: 2, rating: 4 },
+                    { hotel_id: 2, client_id: 3, rating: 3 },
+                ],
+            },
+            {
+                id: 3,
+                name: "Cachorros Hotel",
+                ratings: [{ hotel_id: 3, client_id: 1, rating: 3 },
+                    { hotel_id: 3, client_id: 2, rating: 5 },
+                    { hotel_id: 3, client_id: 3, rating: 4 }
+                ],
+            },
+        ];
 
-    expect(hotels.getHotelsWithRatings()).toEqual(expected);
-  });
+        expect(hotels.getHotelsWithRatings()).toEqual(expected);
+    });
 
-  it("get hotels with average ratings", () => {
 
-  });
+    it("get hotels with average ratings", () => {
+        //what expect
+
+        const expected = [
+            { id: 1, name: 'Thiago Hotel', average: 4.6 },
+            { id: 2, name: 'Brucelherme Hotel', average: 7 },
+            { id: 3, name: 'Cachorros Hotel', average: 9.3 },
+        ]
+        expect(hotels.getHotelsAverageRatings()).toEqual(expected);
+    });
 });
